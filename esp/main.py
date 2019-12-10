249ca5c42f06b31d672e1ee54b5ed40f2b9c5d19
Oleksandr Ivanchuk <sashkoiv@gmail.com>
2019-12-10 18:43:12 +0200

    Refactoring radiator flow

--- esp/main.py
+++ esp/main.py
@@ -7,18 +7,19 @@ import lm75a
 
 
 CONFIGFILE = 'config.json'
-treshold = 28.0
+treshold = 24.0
 last_message = 0
 message_interval = 5
 
-# recover previous status after reboot
+# EXPERIMENTAL
+# Initualize GPIO with before-reboot status
 with open('last_status.txt')as f:
     led_v, relay_v = f.read().split('<&>')
 
-# initualize GPIO with before-reboot status
-led = machine.Pin(2, machine.Pin.OUT, value=led_v)
+led = machine.Pin(13, machine.Pin.OUT, value=led_v)
 relay = machine.Pin(12, machine.Pin.OUT, value=relay_v)
 
+# Read configurations
 with open(CONFIGFILE) as f:
     raw = json.load(f)
     config = raw['mqtt']
@@ -28,31 +29,45 @@ with open(CONFIGFILE) as f:
 
 
 def sub_cb(topic: str, msg: str) -> None:
+    '''
+    Trigerred on message came.
+    Performs action when the message has corresponding Topic.
+    TODO: FIX -> temperature overrides manual On/Off.
+    '''
     if topic == config['sub_topic'].encode():
+        msg = msg.decode()
+        print(msg)
 
-        if msg.decode() == '1':
+        if msg == '1':
             led.value(0)
             relay.value(1)
-        elif msg.decode() == '0':
+            print('ON manually')
+        elif msg == '0':
             led.value(1)
             relay.value(0)
-        else:
-            temp = float(msg.decode())
-            print(temp)
+            print('OFF manually')
+        elif msg.split(' ')[0] == 'temp':
+            temp = float(msg.split(' ')[1].strip('C'))
             if temp < treshold:
                 led.value(0)
                 relay.value(1)
+                print('ON auto')
             elif temp >= treshold:
                 led.value(1)
                 relay.value(0)
-            else:
-                print('Incorrect message -> '.format(msg))
+                print('OFF auto')
+        else:
+            print('Incorrect message -> {}'.format(msg))
 
+    # EXPERIMENTAL
     with open('last_status.txt', 'w')as f:
         f.write('{}<&>{}'.format(led.value(), relay.value()))
 
 
-def connect_and_subscribe():
+def connect_and_subscribe() -> object:
+    '''
+    Setting up client and returns object to interract with.
+    '''
     client_id = ubinascii.hexlify(machine.unique_id())
     client = MQTTClient(
         client_id,
@@ -68,7 +83,10 @@ def connect_and_subscribe():
     return client
 
 
-def restart_and_reconnect():
+def restart_and_reconnect() -> None:
+    '''
+    In case of exception restart the module.
+    '''
     print('Failed to connect to MQTT broker. Reconnecting...')
     time.sleep(10)
     machine.reset()
@@ -91,9 +109,11 @@ while True:
         # Publish routine
         if (time.time() - last_message) > message_interval:
             if temp:
-                msg = str(temp)
-            else:
-                msg = client.publish(config['pub_topic'], msg)
-                last_message = time.time()
+                msg = 'temp {}C'.format(temp)
+            else:   # not needed but to show the module is alive
+                msg = 'ping'
+
+            client.publish(config['pub_topic'], msg)
+            last_message = time.time()
     except OSError as e:
         restart_and_reconnect()
